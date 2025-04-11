7440b7690c08c795bc6e629c8beec6e00c5d66ca
João Gabriel Silva <jgoliveira445@gmail.com>
2025-04-11 14:43:20 -0300

    feat: na tela de game detalis trazendo as informações da tabela user vindas do banco

--- src/app/gamedetails/page.tsx
+++ src/app/gamedetails/page.tsx
@@ -1,35 +1,84 @@
+'use client';
+
 import Header from '@/components/Header/Header';
 import '../../styles/globals.css'
 import styles from './styles.module.css'
 import GameDetailPanel from "@/components/GameDetailPanel/GameDetailPanel";
 import Footer from '@/components/Footer/Footer';
 
+import { useEffect, useState } from 'react';
+
 export default function GameDetails() {
+    const [userData, setUserData] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
+
+    const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (!token) return;
+
+        fetch(`${API_URL}/user-session/`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                const draws = data.total_games - (data.wins + data.losses);
+                const winRate = ((data.wins / data.total_games) * 100).toFixed(2);
+                const lossRate = ((data.losses / data.total_games) * 100).toFixed(2);
+                const rating = data.rating
+
+                setUserData({
+                    ...data,
+                    draws,
+                    winRate: `${winRate}%`,
+                    lossRate: `${lossRate}%`,
+                    rating
+                });
+
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Erro ao buscar dados do usuário:", error);
+                setLoading(false);
+            });
+    });
+
+    if (loading || !userData) {
+        return (
+            <div className={styles.spinnerContainer}>
+                <div className={styles.spinner}></div>
+            </div>
+        );
+    }
+
     return (
         <div className={styles.container}>
-            <Header></Header>
+            <Header />
             <div className={styles.content}>
                 <p>Informações do jogador</p>
-                <GameDetailPanel labels={['Número de partidas', 'Vitórias', 'Derrotas', 'Empates', 'Rating']} values={['10','1','9','0','102']} maxWidth='90%' minWidth='90%'></GameDetailPanel>
+                <GameDetailPanel
+                    labels={['Número de partidas', 'Vitórias', 'Derrotas', 'Empates', 'Rating']}
+                    values={[
+                        userData.total_games.toString(),
+                        userData.wins.toString(),
+                        userData.losses.toString(),
+                        userData.draws.toString(),
+                        userData.rating.toString()
+                    ]}
+                    maxWidth='90%'
+                    minWidth='90%'
+                />
 
                 <p>Outros dados</p>
                 <div className={styles.gameData}>
-                <GameDetailPanel labels={['Taxa de vitória']} values={['10%']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Jogadas espetaculares']} values={['2']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Mates']} values={['1']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Taxa de derrota']} values={['90%']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Erros']} values={['297']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Mates sofridos']} values={['9']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Vitórias (fácil)']} values={['1']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Vitórias (médio)']} values={['0']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Vitórias (difícil)']} values={['0']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Derrotas (fácil)']} values={['3']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Derrotas (médio)']} values={['2']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-                <GameDetailPanel labels={['Derrotas (difícil)']} values={['4']} maxWidth='30%' minWidth='30%'></GameDetailPanel>
-
+                    <GameDetailPanel labels={['Taxa de vitória']} values={[userData.winRate]} maxWidth='30%' minWidth='30%' />
+                    <GameDetailPanel labels={['Taxa de derrota']} values={[userData.lossRate]} maxWidth='30%' minWidth='30%' />
                 </div>
             </div>
-            <Footer iconName='person.svg' text='Perfil'></Footer>
+            <Footer iconName='person.svg' text='Perfil' />
         </div>
     )
 }
