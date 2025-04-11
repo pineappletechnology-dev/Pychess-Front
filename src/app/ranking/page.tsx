138ad3f473acf68b758befa58a0f799b72e07860
João Gabriel Silva <jgoliveira445@gmail.com>
2025-04-11 12:26:22 -0300

    feat: logica implementada para trazer o ranking

--- src/app/ranking/page.tsx
+++ src/app/ranking/page.tsx
@@ -1,30 +1,74 @@
+'use client';
+
 import '../../styles/globals.css'
 import Header from '@/components/Header/Header'
 import styles from './styles.module.css'
 import Footer from '@/components/Footer/Footer'
 import RankItem from '@/components/RankItem/RankItem'
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
+
+interface User {
+    username: string;
+    rating: number;
+}
 
 export default function Ranking() {
-    return(
+    const API_URL = process.env.NEXT_PUBLIC_API_URL;
+    const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState<User[]>([]);
+
+
+    useEffect(() => {
+        fetch(`${API_URL}/get-users/`)
+            .then(res => res.json())
+            .then(data => setUsers(data))
+            .catch(err => console.error('Erro ao buscar usuários:', err))
+            .finally(() => setLoading(false));
+    });
+
+    if (loading) {
+        return (
+            <div className={styles.container}>
+                <Header />
+                <div className={styles.spinnerContainer}>
+                    <div className={styles.spinner}></div>
+                </div>
+                <Footer iconName='crown.svg' text='Ranking' />
+            </div>
+        );
+    }
+
+    const topUser = users[0];
+    const topFive = users.slice(1, 6);
+
+    return (
         <div className={styles.container}>
             <Header></Header>
             <div className={styles.content}>
-                <div className={styles.bestContainer}>
-                    <div className={styles.bestName}>
-                        <p className={styles.bestIcon}><Image src={`/icons/crown.svg`} alt="Profile" width={50} height={50} /></p>
-                        <p className={styles.bestTitle}>Nome do top 1</p>
+                {topUser && (
+                    <div className={styles.bestContainer}>
+                        <div className={styles.bestName}>
+                            <p className={styles.bestIcon}>
+                                <Image src={`/icons/crown.svg`} alt="Profile" width={50} height={50} />
+                            </p>
+                            <p className={styles.bestTitle}>{topUser.username}</p>
+                        </div>
+                        <div className={styles.bestDesc}>
+                            <p className={styles.bestText}>{topUser.rating} pontos</p>
+                        </div>
                     </div>
-                    <div className={styles.bestDesc}>
-                        <p className={styles.bestText}>10 trilhões de vitórias</p>
-                    </div>
-                </div>
+                )}
 
                 <p>Top 5</p>
                 <div className={styles.topPlayersContainer}>
-                    <RankItem name='Fulano da Silva' victoryCount='1 milhão de vitórias'></RankItem>
-                    <RankItem name='Fulano da Silva' victoryCount='1 milhão de vitórias'></RankItem>
-                    <RankItem name='Fulano da Silva' victoryCount='1 milhão de vitórias'></RankItem>
+                    {topFive.map((user, index) => (
+                        <RankItem
+                            key={index}
+                            name={user.username}
+                            victoryCount={`${user.rating} pontos`}
+                        />
+                    ))}
                 </div>
 
             </div>
